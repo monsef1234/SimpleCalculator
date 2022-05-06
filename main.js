8e96c194b0388e55899bd18b158407ec247a497b
monsef <monsef@example.com>
2022-05-06 01:12:25 +0100

    add some features

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+let clipboard1 = new ClipboardJS(".btn");
+let btn = document.querySelector(".btn");
 const result = document.getElementById("result");
 const clearResult = document.getElementById("clear");
 const buttons = document.querySelectorAll("#button");
 const equal = document.getElementById("equal");
 const operators = document.querySelectorAll("#num-first");
+const removeCalc = document.getElementById("remove");
 buttons.forEach((button) => {
   button.addEventListener("click", (eo) => {
-    result.value += button.value;
+    if (result.classList.contains("result")) {
+      result.classList.remove("result");
+      result.value = "";
+      result.value += button.value;
+    } else {
+      result.value += button.value;
+    }
   });
 });
 operators.forEach((operator) => {
@@ -24,17 +33,19 @@ equal.addEventListener("click", (eo) => {
   if (result.value === "") {
     alert("Please Write a calculation");
   } else {
-    const hi = eval(result.value);
-    result.value = hi;
+    const resultCalc = eval(result.value);
+    result.value = resultCalc;
+    result.classList.add("result");
   }
 });
-result.addEventListener("click", (eo) => {
+removeCalc.addEventListener("click", (eo) => {
+  const resultRemove = result.value;
+  result.value = resultRemove.slice(0, resultRemove.length - 1);
+});
+btn.addEventListener("click", (eo) => {
   if (result.value === "") {
     alert("Please Write a calculation");
   } else {
-    alert("Copied !");
-    setTimeout(() => {
-      navigator.clipboard.writeText(result.value);
-    }, 50);
+    alert("Copied!");
   }
 });
